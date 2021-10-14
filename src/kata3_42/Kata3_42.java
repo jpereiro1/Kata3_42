052d5a3f063f8ca458b3716efbd6df19a9447cb9
Entrar <Entrar@PC-1117>
2021-10-14 09:36:13 +0100

    Kata 3, Version 1

---
+++ src/kata3_42/Kata3_42.java
@@ -0,0 +1,10 @@
+package kata3_42;
+
+public class Kata3_42 {
+
+    public static void main(String[] args) {
+        HistogramDisplay histo = new HistogramDisplay("Histograma");
+        histo.execute();
+    }
+    
+}
